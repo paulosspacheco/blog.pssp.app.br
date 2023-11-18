7069d5c296060cef9c1b24557e65cf72f30729b7
paulosspacheco <paulosspacheco@yahoo.com.br>
2023-11-17 21:39:51 -0300

    Atualizado o mapa do site

---
+++ Hidraulica/tipuesearch_content.js
@@ -0,0 +1,5 @@
+var tipuesearch = {
+  pages: [
+  {url: "estudo_relacao_aumento_ pressao_diametro_do_cano.html", title: "Estudar qual a relação do aumento de pressão e diâmetro do cano", text: "", tags: "Água altura área áreas aumento banho bernoulli boia caixa cano carga coluna conforto constante continuidade curva densidade dentro diâmetro diferencial dinâmica disponível dois encontra entre equação escoamento escrita estática estudar exercida fluido fluído fluxo forma gravidade hidráulica indica instrumento líquido localizada medição metros movimento normalmente onde para parada percorrer perda ponto pontos pressão produzem quaisquer qual qualidade quando reduções relação relaciona rhoρrho saída seguinte seja superior torneira trajeto tubo tubos tubulação variação variáveis vazão velocidade venturi " }
+  ]
+};
